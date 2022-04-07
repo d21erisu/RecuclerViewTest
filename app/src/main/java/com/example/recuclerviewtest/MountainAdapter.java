884df8bfaad13edc6ae799506b161a7f7d80bf9f
Erik <d21erisu@student.his.se>
2022-04-07 10:45:46 +0200

    implemented adapterviewholder

--- app/src/main/java/com/example/recuclerviewtest/MountainAdapter.java
+++ app/src/main/java/com/example/recuclerviewtest/MountainAdapter.java
@@ -1,31 +1,44 @@
 package com.example.recuclerviewtest;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MountainAdapter extends RecyclerView.Adapter<MountainAdapter.MountainViewHolder>{
+    private List<String> mountains;
+
+    public MountainAdapter(List<String> mountains) {
+        this.mountains = mountains;
+    }
+
     @NonNull
     @Override
     public MountainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+        return new MountainViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MountainViewHolder holder, int position) {
-
+       holder.mountain_name.setText(mountains.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mountains.size();
     }
 
     public class MountainViewHolder extends RecyclerView.ViewHolder {
+        private TextView mountain_name;
         public MountainViewHolder(@NonNull View itemView) {
             super(itemView);
+            mountain_name = itemView.findViewById(R.id.mountain_name);
         }
     }
 }
